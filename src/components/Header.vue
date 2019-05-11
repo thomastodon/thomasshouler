b0f54f1d981a0815223fae488ef4324722fad907
Thomas Shouler <thomasshouler@Thomass-iMac.local>
2019-05-11 16:55:42 -0500

    Fix everything

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="header">
     <a class="title">thomas shouler</a>
-    <img class=single-line src="/src/assets/single_line.png">
+    <img class=single-line src="single_line.png">
   </div>
 </template>
 
 <script>
+  require('../assets/single_line.png');
   export default {
     name: 'ts-header',
   }
 </script>
 
-<style>
+<style lang="scss">
   img.single-line {
     width: 100%;
     height: 1px;
---
+++ webpack.common.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  stats: {
+    all: false,
+    assets: true,
+    colors: true,
+    errors: true,
+    errorDetails: true,
+    moduleTrace: true,
+    performance: true,
+    warnings: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: 'index.html', title: 'Production'}),
+    new VueLoaderPlugin(),
+    new CleanWebpackPlugin(['dist']),
+  ],
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'build.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader'
+        ],
+      }, {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {}
+          // other vue-loader options go here
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]'
+        }
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    },
+    extensions: ['*', '.js', '.vue', '.json']
+  },
+  performance: {
+    hints: false
+  }
+};
