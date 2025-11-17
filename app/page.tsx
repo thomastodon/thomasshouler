b7f20515bd2f1c20bb61f9be9f8d812da1a50739
thomastodon <thomasshouler@gmail.com>
2025-11-17 07:49:10 -0500

    language

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
               I have a formal background in civil and structural engineering: studying in school for five years with a focus on architecture, followed by four years of professional practice. While practicing, I became increasingly interested in the role technology was playing in developing our physical environment, and began to spend more and more of my time trying to improve the tools we relied on as design engineers.
             </p>
             <p style={{ ...paragraphStyle, marginTop: '1rem' }}>
-              After teaching myself what I could, learning how to build and ship software, I decided to step back from the building industry to focus on this exclusively. Since then, I've worked in a breadth of different spaces, helping our government [and some enterprise] solve some of their most mission critical problems with software. With this experience, I've recently brought my focus back to how we can better construct and operate our built world with technology.
+              After teaching myself what I could, learning how to build and ship software, I decided to step back from the building industry to focus on this exclusively. Since then, I've worked in a breadth of different spaces, helping our government [and some enterprise] solve some of their most mission critical problems with software. I've recently brought my focus back to how we can better construct and operate our built world with technology.
             </p>
             <p style={{ ...paragraphStyle, marginTop: '1rem' }}>
               I live and work in New York City. Contact me at{' '}
